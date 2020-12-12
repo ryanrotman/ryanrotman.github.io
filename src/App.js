c33f4f6ab5e5aef2ac05042c051dc745345fd239
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-12 10:36:15 -0600

    added router paths

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import Navbar from "./components/Navbar";
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Navbar />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/portfolio">
+          <Portfolio />
+        </Route>
+        <Route exact path="/about">
+          <About />
+        </Route>
+        <Route exact path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
