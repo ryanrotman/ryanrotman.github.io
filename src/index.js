7f56e0f3a8cf496f479614935024133786d407c1
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-12 16:31:35 -0600

    added import of materialize js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "materialize-css/dist/css/materialize.min.css";
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "materialize-css/dist/js/materialize";
 
 ReactDOM.render(
   <React.StrictMode>
