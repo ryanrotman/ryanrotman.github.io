7fbaf2dd425347895e8112d30c1ac2e962ee0430
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-12 10:37:04 -0600

    edited hrefs in navbar sections

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,11 +2,11 @@ function Navbar() {
     return (
         <nav className="black" role="navigation">
             <div className="nav-wrapper container">
-            <a id="logo-container" className="brand-logo">ryan<strong>rotman</strong></a>
+            <a href="/" id="logo-container" className="brand-logo">ryan<strong>rotman</strong></a>
             <ul className="right hide-on-med-and-down">
-                <li><a href="#portfolio">Portfolio</a></li>
-                <li><a href="#about-me-heading">About Me</a></li>
-                <li><a href="#skills-connect-resume">Connect</a></li>
+                <li><a href="/portfolio">Portfolio</a></li>
+                <li><a href="/about">About Me</a></li>
+                <li><a href="/contact">Connect</a></li>
             </ul>
 
             <ul id="nav-mobile" className="right sidenav">
