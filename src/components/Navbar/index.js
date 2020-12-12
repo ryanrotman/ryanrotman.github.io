4393c2ead694b8973d41b30a7f616f2c3c8b8971
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-12 16:32:31 -0600

    added home link and edited sidenav sections

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,18 +1,31 @@
+import { useEffect } from "react";
+import M from "materialize-css"
+
 function Navbar() {
+
+    useEffect(() => {
+        document.addEventListener('DOMContentLoaded', function() {
+            let elems = document.querySelectorAll('.sidenav');
+            M.Sidenav.init(elems, options);
+          });
+    }, []);
+
     return (
         <nav className="black" role="navigation">
             <div className="nav-wrapper container">
             <a href="/" id="logo-container" className="brand-logo">ryan<strong>rotman</strong></a>
             <ul className="right hide-on-med-and-down">
+                <li><a href="/">Home</a></li>
                 <li><a href="/portfolio">Portfolio</a></li>
                 <li><a href="/about">About Me</a></li>
                 <li><a href="/contact">Connect</a></li>
             </ul>
 
             <ul id="nav-mobile" className="right sidenav">
-                <li className="white-text"><a href="#portfolio">Portfolio</a></li>
-                <li className="white-text"><a href="#about-me-heading">About Me</a></li>
-                <li className="white-text"><a href="#skills-connect-resume">Connect</a></li>
+                <li className="white-text"><a href="/">Home</a></li>
+                <li className="white-text"><a href="/portfolio">Portfolio</a></li>
+                <li className="white-text"><a href="/about">About Me</a></li>
+                <li className="white-text"><a href="/contact">Connect</a></li>
             </ul>
             <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
             </div>
