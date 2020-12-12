d3c8d83948989ca0eedf6d6d65a962f9d76475e5
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-11 23:34:49 -0600

    added code from current live portfolio site

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,23 @@
+function Navbar() {
+    return (
+        <nav className="black" role="navigation">
+            <div className="nav-wrapper container">
+            <a id="logo-container" className="brand-logo">ryan<strong>rotman</strong></a>
+            <ul className="right hide-on-med-and-down">
+                <li><a href="#portfolio">Portfolio</a></li>
+                <li><a href="#about-me-heading">About Me</a></li>
+                <li><a href="#skills-connect-resume">Connect</a></li>
+            </ul>
+
+            <ul id="nav-mobile" className="right sidenav">
+                <li className="white-text"><a href="#portfolio">Portfolio</a></li>
+                <li className="white-text"><a href="#about-me-heading">About Me</a></li>
+                <li className="white-text"><a href="#skills-connect-resume">Connect</a></li>
+            </ul>
+            <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
