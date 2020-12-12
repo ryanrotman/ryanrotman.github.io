ac1ab960032a46053fe4274b3fbc377b372bb227
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-12 14:35:33 -0600

    added code for footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+function Footer() {
+    return (
+        <div className="footer-copyright">
+            <div className="container center-align">
+                Copyright © 2020 Ryan Rotman
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
