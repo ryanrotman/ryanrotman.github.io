906e801e2dbed9104b06cbdc8e9e7a6f4ad15038
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-13 00:28:15 -0600

    added skills section; FIXME: icons need formatting

---
+++ src/components/Skills/index.js
@@ -0,0 +1,39 @@
+function Skills() {    
+    return (
+        <div className="col s12">
+            <h5 className="white-text" id="skills-heading">Skills</h5>
+            <div className="white-text" style={{fontSize: 70}}>
+                {/* Languages */}
+                <i className="devicon-html5-plain"></i>
+                <i className="devicon-css3-plain"></i>
+                <i className="devicon-javascript-plain"></i>
+                <i className="devicon-jquery-plain"></i>
+                {/* Frameworks */}
+                <i className="devicon-nodejs-plain"></i>
+                <i className="devicon-express-original"></i>
+                <i className="devicon-react-original"></i>
+                <i className="devicon-bootstrap-plain"></i>
+                <i className="devicon-handlebars-plain"></i>
+                <i className="devicon-sequelize-plain"></i>
+                {/* Database */}
+                <i className="devicon-mysql-plain"></i>
+                <i className="devicon-mongodb-plain"></i>
+                {/* Tools/Tech */}
+                <i className="devicon-git-plain"></i>
+                <i className="devicon-github-plain"></i>
+                <i className="devicon-visualstudio-plain"></i>
+                <i className="devicon-npm-original-wordmark"></i>
+                <i className="devicon-heroku-original"></i>
+                <i className="devicon-babel-plain"></i>
+                <i className="devicon-webpack-plain"></i>
+                <i className="devicon-travis-plain"></i>
+                <i className="devicon-slack-plain"></i>
+                {/* Operating Systems */}
+                <i className="devicon-apple-original"></i>
+                <i className="devicon-chrome-plain"></i>
+            </div>
+        </div>
+    )
+}
+
+export default Skills;
