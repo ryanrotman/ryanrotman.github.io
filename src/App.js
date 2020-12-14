e498bfd96e7c874cbdfb26b3dc980910326cc239
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-13 20:35:06 -0600

    added initial state as list of project details to build project cards

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Navbar from "./components/Navbar";
 import './App.css';
 import { BrowserRouter, Route, Switch } from "react-router-dom";
@@ -9,15 +10,64 @@ import Footer from "./components/Footer";
 
 function App() {
 
+  const [projects] = useState([
+    {
+    title: "SHAKR App",
+    description: "A cocktail generator web-app that uses theCocktailDB.com and Google Maps API to render cocktail recipes based on user-submitted ingredients on-hand.",
+    app: "https://abraspin.github.io/Project-1-The-Silent-Marvels-/",
+    code: "https://github.com/abraspin/Project-1-The-Silent-Marvels-",
+    screenshot: "./assets/images/PortfolioPage_Screenshot_Project1.png"
+    },
+    {
+    title: "It's a Blog's Life",
+    description: "A full-stack weblog app built on NodeJS using express-sequelize, handlebars, Passport, and ESLint. Share your thoughts, find new friends, and give feedback to others content in the form of comments and likes!",
+    app: "https://its-a-blogs-life.herokuapp.com/",
+    code: "https://github.com/Cenzo-cmd/Project-2",
+    screenshot: "./assets/images/PortfolioPage_Screenshot_Project2.png"
+    },
+    {
+    title: "Reading List",
+    description: "A full-stack app built on MongoDB, Express, React, and Node that allows a user to add a book, author, and synopsis to their list of books they want to read.",
+    app: "https://rr-react-reading-list.herokuapp.com/",
+    code: "https://github.com/ryanrotman/react-reading-list",
+    screenshot: "./assets/images/PortfolioPage_Screenshot_ReadingList.png"
+    },
+    {
+    title: "Eat-Da-Burger",
+    description: "Burger logger that allows a user to create a burger, change its devoured status, and delete a burger. This app uses Node, Express, Handlebars, and MySQL.",
+    app: "https://rr-eat-da-burger.herokuapp.com",
+    code: "https://github.com/ryanrotman/homework-13-eat-da-burger",
+    screenshot: "./assets/images/PortfolioPage_ScreenShot_Homework13.png"
+    },
+    {
+    title: "Note Taker",
+    description: "This an express application that allows a user to write notes, view submitted notes, and delete notes.",
+    app: "https://radiant-tor-19120.herokuapp.com",
+    code: "https://github.com/ryanrotman/homework-11-note-taker",
+    screenshot: "./assets/images/PortfolioPage_ScreenShot_Homework11.png"
+    },
+    {
+    title: "Weather Dashboard",
+    description: "Weather dashboard that incorporates third-party APIs using jQuery's AJAX call to show a city's current temperature, humidity, wind speed, uv index and 5-day forecast. Uses Local Storage for persistent data.",
+    app: "https://ryanrotman.github.io/homework-6-weather-dashboard/",
+    code: "https://github.com/ryanrotman/homework-6-weather-dashboard",
+    screenshot: "./assets/images/PortfolioPage_Screenshot_Homework6.png"
+    }
+  ]);
+
   return (
     <BrowserRouter>
       <Navbar />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home
+            project={projects}
+          />
         </Route>
         <Route exact path="/portfolio">
-          <Portfolio />
+          <Portfolio
+            project={projects}
+          />
         </Route>
         <Route exact path="/about">
           <About />
