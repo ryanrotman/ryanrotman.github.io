718f7c5adadf30c6eff3db9bfaba34231297ad36
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-13 23:24:38 -0600

    changed class to className

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -6,19 +6,19 @@ function Project(props) {
         <div id="project-cards">
             {props.project.map(project => {
                 return (
-                    <div key={project.id} class="card sticky-action hoverable">
-                        <div class="card-image waves-effect waves-block waves-light">
-                            <img class="activator" src={project.screenshot} />
+                    <div key={project.id} className="card sticky-action hoverable">
+                        <div className="card-image waves-effect waves-block waves-light">
+                            <img className="activator" src={project.screenshot} />
                         </div>
-                        <div class="card-content left-align">
-                            <span class="card-title activator grey-text text-darken-4"><strong>{project.title}</strong></span>
+                        <div className="card-content left-align">
+                            <span className="card-title activator grey-text text-darken-4"><strong>{project.title}</strong></span>
                         </div>
-                        <div class="card-action center">
-                            <a class="waves-effect waves-light btn" href={project.app} target="_blank">View App</a>
-                            <a class="waves-effect waves-light btn" href={project.code} target="_blank">View Code</a>
+                        <div className="card-action center">
+                            <a className="waves-effect waves-light btn" href={project.app} target="_blank">View App</a>
+                            <a className="waves-effect waves-light btn" href={project.code} target="_blank">View Code</a>
                         </div>
-                        <div class="card-reveal left-align">
-                            <span class="card-title grey-text text-darken-4"><strong>{project.title}</strong><i class="material-icons right">close</i></span>
+                        <div className="card-reveal left-align">
+                            <span className="card-title grey-text text-darken-4"><strong>{project.title}</strong><i className="material-icons right">close</i></span>
                             <p>{project.description}</p>
                         </div>
                     </div>
