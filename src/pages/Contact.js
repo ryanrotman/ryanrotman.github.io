f71efbec027bc24319c09b105bfffe5fc09a26a0
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-15 15:42:39 -0600

    added google contact form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -39,6 +39,8 @@ function Contact() {
                     </div>
                 </div>
             </div>
+            <br />
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdT-EMTv2eNQ4m60617TdLgIcTZXQpnpqukQpLy8IwM4p06RQ/viewform?embedded=true" width="100%" height="840" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </div>
     );
 }
