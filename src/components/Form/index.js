a16216462a4dfa6f28516ab6989ed9ffc3544d70
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-16 21:35:57 -0600

    built Form component

---
+++ src/components/Form/index.js
@@ -0,0 +1,9 @@
+function Form() {
+    return (
+        <div id="contact-form-section">
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdT-EMTv2eNQ4m60617TdLgIcTZXQpnpqukQpLy8IwM4p06RQ/viewform?embedded=true" width="100%" height="840" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+        </div>
+    )
+}
+
+export default Form;
