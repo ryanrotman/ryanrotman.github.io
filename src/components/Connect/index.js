de8685276e4ea490aacc18d297f7837495eaf963
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-16 21:35:10 -0600

    built Connect component

---
+++ src/components/Connect/index.js
@@ -0,0 +1,14 @@
+function Connect() {
+    return (
+        <div id="connect-section">
+            <h5 className="white-text" id="connect-heading">Connect</h5>
+            <div style={{fontSize: 35}}>
+                <a href="https://github.com/ryanrotman" target="_blank" title="View my GitHub profile"><i className="fab fa-github hvr-grow white-text"></i></a>
+                &nbsp;
+                <a href="https://www.linkedin.com/in/ryanrotman/" target="_blank" title="Connect with me on LinkedIn"><i className="fab fa-linkedin hvr-grow white-text"></i></a>
+            </div>
+        </div>
+    )
+}
+
+export default Connect;
