9d94dbdd9469dd0b22ec3b40b3a4b527e8b33739
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-16 21:34:13 -0600

    built Resume component

---
+++ src/components/Resume/index.js
@@ -0,0 +1,14 @@
+import pdf from "../../assets/RyanRotman_Resume.pdf";
+
+function Resume() {
+    return (
+        <div id="resume-section">
+            <h5 className="white-text" id="resume-heading">Resume</h5>
+            <div style={{fontSize: 35}}>
+                <a href={pdf} target="_blank" title="Download my resume"><i className="fas fa-file-download hvr-grow white-text"></i></a>
+            </div>
+        </div>
+    )
+}
+
+export default Resume;
