1a8267e5e19a220ad0f76c9337349144ff94e541
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-16 21:27:53 -0600

    added projects heading and note from Portfolio page

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -8,27 +8,31 @@ function Project(props) {
     console.log("REVEAL STATUS: ", isShown);
 
     return (
-        <div id="project-cards">
-            {props.project.map(project => {
-                return (
-                    <div key={project.id} className="card sticky-action hoverable" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
-                        <div className="card-image waves-effect waves-block waves-light">
-                            <img className="activator" src={project.screenshot} />
+        <div className="center-align" id="project-section">
+            <h5 className="white-text" id="projects-heading">Projects</h5>
+            <p className="white-text center-align" id="projects-note"><i>(click on the image to see a description of the app)</i></p>
+            <div id="project-cards">
+                {props.project.map(project => {
+                    return (
+                        <div key={project.id} className="card sticky-action hoverable" onMouseEnter={() => setIsShown(true)} onMouseLeave={() => setIsShown(false)}>
+                            <div className="card-image waves-effect waves-block waves-light">
+                                <img className="activator" id="project-screenshot" src={project.screenshot} />
+                            </div>
+                            <div className="card-content left-align">
+                                <span className="card-title activator grey-text text-darken-4"><strong>{project.title}</strong></span>
+                            </div>
+                            <div className="card-action center">
+                                <a className="waves-effect waves-light btn" href={project.app} target="_blank">View App</a>
+                                <a className="waves-effect waves-light btn" href={project.code} target="_blank">View Code</a>
+                            </div>
+                            {isShown ? <div className="card-reveal left-align">
+                                <span className="card-title grey-text text-darken-4"><strong>{project.title}</strong><i className="material-icons right">close</i></span>
+                                <p>{project.description}</p>
+                            </div> : ""}
                         </div>
-                        <div className="card-content left-align">
-                            <span className="card-title activator grey-text text-darken-4"><strong>{project.title}</strong></span>
-                        </div>
-                        <div className="card-action center">
-                            <a className="waves-effect waves-light btn" href={project.app} target="_blank">View App</a>
-                            <a className="waves-effect waves-light btn" href={project.code} target="_blank">View Code</a>
-                        </div>
-                        {isShown ? <div className="card-reveal left-align">
-                            <span className="card-title grey-text text-darken-4"><strong>{project.title}</strong><i className="material-icons right">close</i></span>
-                            <p>{project.description}</p>
-                        </div> : ""}
-                    </div>
-                )
-            })}
+                    )
+                })}
+            </div>
         </div>
     )
     
