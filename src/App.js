9eba068876f5e2ee9e8207b947ce3c8333a1753a
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-17 23:48:27 -0600

    removed separate card states; ended up not needing to do it that way

--- src/App.js
+++ src/App.js
@@ -17,61 +17,6 @@ import "materialize-css/dist/css/materialize.min.css";
 
 function App() {
 
-  const [projectCard1] = useState([
-    {
-      title: "SHAKR App",
-      description: "A cocktail generator web-app that uses theCocktailDB.com and Google Maps API to render cocktail recipes based on user-submitted ingredients on-hand.",
-      app: "https://abraspin.github.io/Project-1-The-Silent-Marvels-/",
-      code: "https://github.com/abraspin/Project-1-The-Silent-Marvels-",
-      screenshot: project1Img
-    }
-  ]);
-  const [projectCard2] = useState([
-    {
-      title: "It's a Blog's Life",
-      description: "A full-stack weblog app built on NodeJS using express-sequelize, handlebars, Passport, and ESLint. Share your thoughts, find new friends, and give feedback to others content in the form of comments and likes!",
-      app: "https://its-a-blogs-life.herokuapp.com/",
-      code: "https://github.com/Cenzo-cmd/Project-2",
-      screenshot: project2Img
-    }
-  ]);
-  const [projectCard3] = useState([
-    {
-      title: "Reading List",
-      description: "A full-stack app built on MongoDB, Express, React, and Node that allows a user to add a book, author, and synopsis to their list of books they want to read.",
-      app: "https://rr-react-reading-list.herokuapp.com/",
-      code: "https://github.com/ryanrotman/react-reading-list",
-      screenshot: readingListImg
-    }
-  ]);
-  const [projectCard4] = useState([
-    {
-      title: "Eat-Da-Burger",
-      description: "Burger logger that allows a user to create a burger, change its devoured status, and delete a burger. This app uses Node, Express, Handlebars, and MySQL.",
-      app: "https://rr-eat-da-burger.herokuapp.com",
-      code: "https://github.com/ryanrotman/homework-13-eat-da-burger",
-      screenshot: homework13Img
-    }
-  ]);
-  const [projectCard5] = useState([
-    {
-      title: "Note Taker",
-      description: "This an express application that allows a user to write notes, view submitted notes, and delete notes.",
-      app: "https://radiant-tor-19120.herokuapp.com",
-      code: "https://github.com/ryanrotman/homework-11-note-taker",
-      screenshot: homework11Img
-    }
-  ]);
-  const [projectCard6] = useState([
-    {
-      title: "Weather Dashboard",
-      description: "Weather dashboard that incorporates third-party APIs using jQuery's AJAX call to show a city's current temperature, humidity, wind speed, uv index and 5-day forecast. Uses Local Storage for persistent data.",
-      app: "https://ryanrotman.github.io/homework-6-weather-dashboard/",
-      code: "https://github.com/ryanrotman/homework-6-weather-dashboard",
-      screenshot: homework6Img
-    }
-  ])
-
   const [projects] = useState([
     {
     id: 1,
@@ -131,23 +76,11 @@ function App() {
           <Route exact path="/">
             <Home
               project={projects}
-              projectCard1={projectCard1}
-              projectCard2={projectCard2}
-              projectCard3={projectCard3}
-              projectCard4={projectCard4}
-              projectCard5={projectCard5}
-              projectCard6={projectCard6}
             />
           </Route>
           <Route exact path="/portfolio">
             <Portfolio
               project={projects}
-              projectCard1={projectCard1}
-              projectCard2={projectCard2}
-              projectCard3={projectCard3}
-              projectCard4={projectCard4}
-              projectCard5={projectCard5}
-              projectCard6={projectCard6}
             />
           </Route>
           <Route exact path="/about">
