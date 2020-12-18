db191f19fab5c82399233328ade2fe14870add44
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-18 00:04:40 -0600

    added accents to the letter e

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import pdf from "../../assets/RyanRotman_Resume.pdf";
 function Resume() {
     return (
         <div id="resume-section">
-            <h5 className="white-text" id="resume-heading">Resume</h5>
+            <h5 className="white-text" id="resume-heading">Résumé</h5>
             <div style={{fontSize: 35}}>
                 <a href={pdf} target="_blank" title="Download my resume"><i className="fas fa-file-download hvr-grow white-text"></i></a>
             </div>
