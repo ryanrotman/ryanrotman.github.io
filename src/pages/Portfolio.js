30b569b31a5017cba4104ebf5770c49cf964ef68
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-17 23:47:25 -0600

    edited Project section and Skills section layouts

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -9,13 +9,43 @@ function Portfolio(props) {
     }, []);
 
     return (
-        <div className="container portfolio-container center-align">
-            <Project
-            project={props.project}
-            />
+        <div className="container">
+            <div className="center-align" id="project-section">
+                <h5 className="white-text" id="projects-heading">Projects</h5>
+                <div id="project-cards">
+                    <Project
+                    project={props.project[0]}
+                    />
+                    <Project
+                    project={props.project[1]}
+                    />
+                    <Project
+                    project={props.project[2]}
+                    />
+                    <Project
+                    project={props.project[3]}
+                    />
+                    <Project
+                    project={props.project[4]}
+                    />
+                    <Project
+                    project={props.project[5]}
+                    />
+                </div>
+            </div>
             &nbsp;
-            <Skills />
+            <div className="center-align">
+                <Skills />
+            </div>
         </div>
+
+        // <div className="container portfolio-container center-align">
+        //     <Project
+        //     project={props.project}
+        //     />
+        //     &nbsp;
+        //     <Skills />
+        // </div>
     );
 }
 
