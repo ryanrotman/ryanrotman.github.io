7b736b92827a3cf33beb45d32c5819972014a5ab
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-19 20:35:04 -0600

    cleaned up code

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -24,35 +24,6 @@ function Project(props) {
             </div>
         </div>
     )
-
-    // return (
-    //     <div className="center-align" id="project-section">
-    //         <h5 className="white-text" id="projects-heading">Projects</h5>
-    //         <p className="white-text center-align" id="projects-note"><i>(click on the image to see a description of the app)</i></p>
-    //         <div id="project-cards">
-    //             {props.project.map(project => {
-    //                 return (
-    //                     <div key={project.id} className="card sticky-action hoverable" onMouseEnter={() => activator.current.click()} onMouseLeave={() => close.current.click()}>
-    //                         <div className="card-image waves-effect waves-block waves-light">
-    //                             <img className="activator" id="project-screenshot" ref={activator} src={project.screenshot} />
-    //                         </div>
-    //                         <div className="card-content left-align">
-    //                             <span className="card-title activator grey-text text-darken-4"><strong>{project.title}</strong></span>
-    //                         </div>
-    //                         <div className="card-action center">
-    //                             <a className="waves-effect waves-light btn" href={project.app} target="_blank">View App</a>
-    //                             <a className="waves-effect waves-light btn" href={project.code} target="_blank">View Code</a>
-    //                         </div>
-    //                         <div className="card-reveal left-align">
-    //                             <span className="card-title grey-text text-darken-4"><strong>{project.title}</strong><i className="material-icons right" ref={close}>close</i></span>
-    //                             <p>{project.description}</p>
-    //                         </div>
-    //                     </div>
-    //                 )
-    //             })}
-    //         </div>
-    //     </div>
-    // )
     
 }
 
