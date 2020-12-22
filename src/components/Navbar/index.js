af5d5f2f8f7c1fd6db151a61e31418504e472645
Ryan Rotman <ryan.rotman@gmail.com>
2020-12-21 22:04:19 -0600

    changed connect to contact

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
-import M from "materialize-css"
+import { Link } from "react-router-dom";
+import M from "materialize-css";
 
 function Navbar() {
 
@@ -11,19 +12,19 @@ function Navbar() {
     return (
         <nav className="black" role="navigation">
             <div className="nav-wrapper container">
-            <a href="/" id="logo-container" className="brand-logo">ryan<strong>rotman</strong></a>
+            <Link to="/" id="logo-container" className="brand-logo">ryan<strong>rotman</strong></Link>
             <ul className="right hide-on-med-and-down">
-                <li><a href="/">Home</a></li>
-                <li><a href="/portfolio">Portfolio</a></li>
-                <li><a href="/about">About Me</a></li>
-                <li><a href="/contact">Connect</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/portfolio">Portfolio</Link></li>
+                <li><Link to="/about">About Me</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
 
             <ul id="nav-mobile" className="right sidenav">
-                <li className="white-text"><a href="/">Home</a></li>
-                <li className="white-text"><a href="/portfolio">Portfolio</a></li>
-                <li className="white-text"><a href="/about">About Me</a></li>
-                <li className="white-text"><a href="/contact">Connect</a></li>
+                <li className="white-text"><Link to="/">Home</Link></li>
+                <li className="white-text"><Link to="/portfolio">Portfolio</Link></li>
+                <li className="white-text"><Link to="/about">About Me</Link></li>
+                <li className="white-text"><Link to="/contact">Contact</Link></li>
             </ul>
             {/* eslint-disable-next-line */}
             <a href="#" data-target="nav-mobile" className="sidenav-trigger"><i className="material-icons">menu</i></a>
